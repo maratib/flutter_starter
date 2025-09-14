48b0f7b9d0f95d6955aaa6078ee7f547527d7edc
Maratib Ali Khan <maratib@gmail.com>
2025-09-14 15:44:22 +0500

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_starter/pages/home.dart';
+
+class App extends StatelessWidget {
+  final EdgeInsetsGeometry padding = const EdgeInsets.all(16.0);
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Home')),
+        body: SafeArea(
+          child: Padding(padding: padding, child: Home()),
+        ),
+      ),
+    );
+  }
+}
